d740e98c9960bad7aae12602b646691466422fca
Keith N <78460602+KeithLion@users.noreply.github.com>
2021-12-09 11:57:22 -0600

    created bank class and added methods

--- bankuser.py
+++ bankuser.py
@@ -12,3 +12,28 @@ class User:
 
     def display_user_balance(self):
         print(f'User:{self.name}, Balance:{self.account.account}')
+
+
+class BankAccount:
+
+    def __init__(self,int_rate,balance):
+        self.intrest = int_rate
+        self.account = balance
+    
+    def deposit(self,amount):
+        self.account += amount
+    
+    def withdraw(self,amount):
+        if (self.account - amount) > 0:
+            self.account -= amount
+        else:
+            print('Insufficient funds: charging $5 fee')
+            self.account - 5
+
+    def display_account(self):
+        print(f'Balance:{self.account}')
+    
+    def yield_intrest(self):
+        if (self.account > 0):
+            self.account += self.account*0.07
+
